682b93a8395584a20d92f9f4cc851d445c58ce7f
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-17 21:08:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,7 @@
-def prime?(interger)# Add  code here!
-   
+def prime?(value)
+  if value <= 1 || value == 0 || value == 1
+    return false
+  elsif
+    (2..value - 1).each do |i|
+      if value % i == 0
+      return false
